7715eff781e5e38f33fb2c1a91b83b1465607c04
fishman123456 <den9152@yandex.ru>
2023-02-26 17:56:05 +0700

    русские не добавляет

--- sql_connection.cs
+++ sql_connection.cs
@@ -10,7 +10,7 @@ using System.Data.Common;
 
 namespace _26_02_2023_sql_consol_library
 {
-    public class Sql_connect
+    public class Sql_work
     {
         // Метод, который создает SQL-соединение с базой данных
         public static void Create_SQL_Connection()
@@ -107,5 +107,124 @@ namespace _26_02_2023_sql_consol_library
             }
         }
 
+        public static void Add_data_for_table()
+        {
+            /*Объявляем строковую переменную и записываем в нее
+             строку подключения 
+             Data Source - имя сервера, по стандарту (local)\SQLEXPRESS
+             Initial Catalog - имя БД 
+             Integrated Security=-параметры безопасности
+             */
+            Console.WriteLine("запущен метод Add_data_for_table()");
+            // Читает строки из базы данных
+            // 1. Строка запроса
+            string queryStr = "SELECT * FROM [dbo].[Authors]";
+
+            // 2. Строка подключения
+            string connStr = @"Data Source = (localdb)\MSSQLLocalDB; " +
+         "Initial Catalog = Library; Integrated Security = True";
+            /*Здесь указал имя БД(хотя для создания БД его указывать не нужно)
+              для того, чтобы проверить, может данная БД уже создана
+            Создаем экземпляр класса  SqlConnection по имени conn
+            и передаем конструктору этого класса, строку подключения
+             */
+            SqlConnection conn = new SqlConnection(connStr);
+            try
+            {
+                //пробуем подключится
+                conn.Open();
+            }
+            catch (SqlException se)
+            {
+                Console.WriteLine("Ошибка подключения:{0}", se.Message);
+                return;
+            }
+
+            Console.WriteLine("Соедение успешно произведено");
+            /*Создаем экземпляр класса  SqlCommand по имени cmdCreateTable
+             и передаем конструктору этого класса, запрос на 
+             добавление строки в  таблицу Students
+             и объект типа SqlConnection
+            */
+
+            SqlCommand cmd = new SqlCommand("Insert into Authors" +
+            "( FirstName, LastName) Values" +
+            " ( @FirstName, @LastName)", conn);
+            /*Работаем с параметрами(SqlParameter), эта техника позволяет уменьшить
+            кол-во ошибок и достичь большего быстродействия
+             но требует и больших усилий в написании кода*/
+            //объявляем объект класса SqlParameter
+            SqlParameter param = new SqlParameter();
+            
+            //переопределяем объект класса SqlParameter
+            param = new SqlParameter();
+            //задаем имя параметра
+            param.ParameterName = "@FirstName";
+            //задаем значение параметра
+            param.Value = "N 'Карл'";
+            //задаем тип параметра
+            param.SqlDbType = SqlDbType.Text;
+            //передаем параметр объекту класса SqlCommand
+            cmd.Parameters.Add(param);
+
+            //переопределяем объект класса SqlParameter
+            param = new SqlParameter();
+            //задаем имя параметра
+            param.ParameterName = "@LastName";
+            //задаем значение параметра
+            param.Value = "N 'Маркс'";
+            //задаем тип параметра
+            param.SqlDbType = SqlDbType.Text;
+            //передаем параметр объекту класса SqlCommand
+            cmd.Parameters.Add(param);
+
+            Console.WriteLine("Вставляем запись");
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка, при выполнении запроса на добавление записи");
+                return;
+            }
+            //Выводим значение на экран
+            cmd = new SqlCommand("Select * From Authors", conn);
+            /*Метод ExecuteReader() класса SqlCommand возврашает
+             объект типа SqlDataReader, с помошью которого мы можем
+             прочитать все строки, возврашенные в результате выполнения запроса
+             CommandBehavior.CloseConnection - закрываем соединение после запроса
+             */
+            using (SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+            {
+                //цикл по всем столбцам полученной в результате запроса таблицы
+                for (int i = 0; i < dr.FieldCount; i++)
+                    /*метод GetName() класса SqlDataReader позволяет получить имя столбца
+                     по номеру, который передается в качестве параметра, данному методу
+                     и озночает номер столбца в таблице(начинается с 0)
+                     */
+                    Console.Write("{0}\t", dr.GetName(i).ToString().Trim());
+                /*читаем данные из таблицы
+                 чтение происходит только в прямом направлении
+                 все прочитаные строки отбрасываюся */
+                Console.WriteLine();
+                while (dr.Read())
+                {
+                    /*метод GetValue() класса SqlDataReader позволяет получить значение столбца
+                                            по номеру, который передается в качестве параметра, данному методу
+                                            и озночает номер столбца в таблице(начинается с 0)
+                                            */
+                    Console.WriteLine("{0}\t{1}\t{2}", dr.GetValue(0).ToString().Trim(),
+                     dr.GetValue(1).ToString().Trim(),
+                     dr.GetValue(2).ToString().Trim());
+                }
+            }
+            //закрвываем соединение
+            conn.Close();
+            conn.Dispose();
+            Console.WriteLine();
+        }
+
     }
+
 }
